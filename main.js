2d2d5a1b05b08165c41591a5f377ae96698800ee
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-06-21 08:48:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+// Question # 1
+// Write a simple asynchronous TypeScript function fetchGreeting that returns a
+// greeting message after a 2-second delay using setTimeout.
+async function fetchGreeting() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("hello dear john");
+        }, 2000);
+    });
+}
+fetchGreeting().then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
+// Question # 2
+// Write a function simulateTask that simulates a task by logging "Task started",
+// waits for 1 second, and then logs "Task completed". Use setTimeout for the delay.
+let simulateTask = () => {
+    console.log("task started");
+    setTimeout(() => {
+        console.log("task completed");
+    }, 1000);
+};
+simulateTask();
+// Question # 3
+// Write a function fetchData that returns a Promise which resolves with the string
+// "Data fetched successfully!" after a delay of 1 second.
+let fetchData = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("data fetched sucessfully!");
+        }, 1000);
+    });
+};
+fetchData().then((result) => {
+    console.log(result);
+});
+// Question # 4
+// Write a function fetchWithError that returns a Promise. It should randomly either
+// resolve with "Data fetched successfully!" or reject with "Data fetch failed!" after a
+// delay of 1 second. Handle the rejection using .catch
+let fetchWithError = () => {
+    return new Promise((res, rej) => {
+        res("data fetched sucessfully!");
+        rej("data fetch failed!");
+        setTimeout(() => {
+            if (Math.random() > 0.5) {
+                res("data fetched sucessfully!");
+            }
+            else {
+                rej("data fetch failed!");
+            }
+        }, 1000);
+    });
+};
+fetchWithError().catch((error) => {
+    console.log(error);
+}).then((message) => {
+    console.log(message);
+});
+export {};
+// Question # 5
+// Write a function executeSequentially that executes two functions fetchData and
+// processData sequentially using Promises, and logs the results in the order they are
+// called.
+//  let executeSequentially=()=>{
+//     let fetchData=()=>{
+//         return new Promise((res:any,rej:any)=>{
+//             res("data fetch sucessfully!")
+//         })
+//     };
+//     let processData=()=>{
+//         return new Promise((res:any,rej:any)=>{
+//             rej("data processed failed!")
+//         })
+//     }
+// }
+// fetchData().then((result)=>{console.log(result);
+//     processData().catch((error)=>{console.log(error);
+//     })
+// })
